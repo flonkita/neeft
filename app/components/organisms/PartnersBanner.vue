a6b6d6940744af82d1107d6b393280e4354b57fb
florent.nkita <flo95400@gmail.com>
2026-06-22 01:36:42 +0200

    style(banner): Ajout de plusieurs images pour les images partenaires

--- app/components/organisms/PartnersBanner.vue
+++ app/components/organisms/PartnersBanner.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="partners-banner">
-    <img src="/huh.jpg" alt="Logos des partenaires : FFF, Kcorp, République Française, TKR" class="partners-banner__image" />
+    <img src="/huh.jpg" alt="Logos des partenaires : Huh.." class="partners-banner__image" />
+    <img src="/pinch.jpg" alt="Logos des partenaires : Noooon.." class="partners-banner__image" />
+    <img src="/sip.jpg" alt="Logos des partenaires : Boire.." class="partners-banner__image" />
+    <img src="/kneel.jpg" alt="Logos des partenaires : A genoux.." class="partners-banner__image" />
   </div>
 </template>
 
